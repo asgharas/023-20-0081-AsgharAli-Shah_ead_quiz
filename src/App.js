2f0f745b5e82174877f9ba75ab39afede9ad6db8
Asghar Ali Shah <76618200+asgharas@users.noreply.github.com>
2023-11-17 14:40:14 +0500

    Add Navbar, Welcome, Offerings, About, ContactUs, and Footer components to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
+import Offerings from './components/Offerings';
+import { About } from './components/About';
+import { ContactUs } from './components/ContactUs';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>REACT APP</h1>
+      <Navbar />
+      <Welcome />
+      <Offerings />
+      <About />
+      <ContactUs />
+      <Footer />
     </div>
   );
 }
